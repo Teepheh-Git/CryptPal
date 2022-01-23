53597181c552c90e711ac1c2c89f503d36de69d5
Oguntoyinbo Boluwatife <81614588+Teepheh-Git@users.noreply.github.com>
2022-01-23 16:45:51 +0100

    Favourite Section done

--- src/screens/Favorite/index.js
+++ src/screens/Favorite/index.js
@@ -1,18 +1,22 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { connect } from "react-redux";
 import CoinList from "../../components/CoinList";
-import { FONTS, SIZES } from "../../constants";
+import { COLORS, FONTS, SIZES } from "../../constants";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect } from "@react-navigation/native";
+import LinearGradient from "react-native-linear-gradient";
 
 
 const Favorite = ({ appTheme, navigation, appCurrency }) => {
 
   const [favs, setFavs] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [favPageError, setFavPageError] = useState(true);
+  const [retry, setRetry] = useState("");
+
 
 
 
@@ -22,27 +26,29 @@ const Favorite = ({ appTheme, navigation, appCurrency }) => {
       const GetFavorites = async () => {
 
         try {
+          setLoading(true);
+
           const valueGot = await AsyncStorage.getItem("FavoriteCoin");
           const coinGotten = await JSON.parse(valueGot);
-
-          setLoading(true);
           const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${appCurrency.ticker}&ids=${coinGotten[0]}%2C${coinGotten[1]}%2C${coinGotten[2]}%2C${coinGotten[3]}%2C${coinGotten[4]}%2C${coinGotten[5]}%2C${coinGotten[6]}%2C${coinGotten[7]}%2C${coinGotten[8]}%2C${coinGotten[9]}%2C${coinGotten[10]}%2C${coinGotten[11]}%2C${coinGotten[12]}%2C${coinGotten[13]}%2C${coinGotten[14]}%2C${coinGotten[15]}%2C${coinGotten[16]}%2C${coinGotten[17]}%2C${coinGotten[18]}%2C${coinGotten[19]}%2C${coinGotten[20]}%2C${coinGotten[21]}%2C${coinGotten[22]}%2C${coinGotten[23]}%2C${coinGotten[24]}&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d`);
-
           await setFavs(res.data);
 
           if (res) {
             setLoading(false);
+            setFavPageError(false)
+
           }
 
         } catch (e) {
           console.log(e);
           setLoading(false);
+          setFavPageError(true)
 
         }
       };
 
       GetFavorites();
-    }, [appCurrency]),
+    }, [appCurrency, retry]),
   );
 
 
@@ -76,6 +82,37 @@ const Favorite = ({ appTheme, navigation, appCurrency }) => {
   };
 
 
+
+
+  // NETWORK ERROR FUNCTION
+  const NetworkError = () => {
+
+    // RETRY FUNCTION
+    const Retry = () => {
+      setLoading(true);
+      setFavPageError(true);
+      let r = Math.random().toString(36).substr(2, 5);
+      setRetry(r);
+    };
+
+    return (
+      <View style={styles.networkErrorContainer}>
+        <Image style={{ height: 98, width: 98 }} source={require("../../assets/images/ExpressionLess.png")} />
+        <Text style={[styles.networkErrorText, { color: appTheme.textColor }]}>Network error!! </Text>
+        <Text style={[styles.networkErrorDesc, { color: appTheme.textColor3 }]}>Your network is asleep, please check
+          your internet connections and click refresh.</Text>
+        <TouchableOpacity activeOpacity={0.6} onPress={() => Retry()}>
+          <LinearGradient style={styles.refreshButton} colors={["#4F36C4", "#4F36C4"]}>
+            <Text style={styles.refresh}>Refresh</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+
+
+
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: appTheme.backgroundColor2 }]}>
 
@@ -85,21 +122,24 @@ const Favorite = ({ appTheme, navigation, appCurrency }) => {
         </View>
       </View>
 
+      {favPageError ? NetworkError() :
+        (
+          loading ? <ActivityIndicator color={appTheme.textColor2} size={"small"} /> :
+            (favs.length < 1 ? EmptyFavorite() :
+              <FlatList
+                // data={UniqueFavCoins}
+                data={favs}
+                keyExtractor={(_, index) => index.toString()}
+                renderItem={CoinListRenderItem}
+                showsVerticalScrollIndicator={false}
+                initialNumToRender={6}
+                maxToRenderPerBatch={2}
+                windowSize={3}
+              />)
+        )
 
 
-
-      {loading ? <ActivityIndicator color={appTheme.textColor2} size={"small"} /> :
-        (favs.length < 1 ? EmptyFavorite() :
-          <FlatList
-            // data={UniqueFavCoins}
-            data={favs}
-            keyExtractor={(_, index) => index.toString()}
-            renderItem={CoinListRenderItem}
-            showsVerticalScrollIndicator={false}
-            initialNumToRender={6}
-            maxToRenderPerBatch={2}
-            windowSize={3}
-          />)}
+      }
     </SafeAreaView>
   );
 };
@@ -127,6 +167,32 @@ const styles = StyleSheet.create({
   title: {
     ...FONTS.h2,
     marginHorizontal: 5,
+  },
+  refreshButton: {
+    width: SIZES.width * 0.34,
+    height: 48,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: SIZES.radius3,
+    marginVertical: 40,
+  },
+  refresh: {
+    color: COLORS.white,
+    ...FONTS.h5,
+  },
+  networkErrorContainer: {
+    width: SIZES.width * 0.7,
+    alignItems: "center",
+    justifyContent: "center",
+    top: SIZES.height * 0.2,
+  },
+  networkErrorText: {
+    ...FONTS.h4,
+    marginVertical: 5,
+  },
+  networkErrorDesc: {
+    ...FONTS.body4,
+    textAlign: "center",
   },
 
 });
